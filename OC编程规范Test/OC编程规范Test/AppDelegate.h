3203f6f5540d5e8bc58c7d1149bfcb3f427654c1
gaoshan <1328279962@qq.com>
2018-03-20 17:32:14 +0800

    新建工程，主要书写OC编程规范

---
+++ OC编程规范Test/OC编程规范Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OC编程规范Test
+//
+//  Created by most on 2018/3/20.
+//  Copyright © 2018年 most. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OC编程规范Test/OC编程规范Test/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  OC编程规范Test
+//
+//  Created by most on 2018/3/20.
+//  Copyright © 2018年 most. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
